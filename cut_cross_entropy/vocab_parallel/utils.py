Fix vocab parallel loss computation (#42)

cut_cross_entropy/vocab_parallel/utils.py
@@ -15,20 +15,33 @@ def partition_n_into_range(n: int, rank: int, world_size: int) -> tuple[int, int
 
 @dataclass
 class VocabParallelOptions:
+    """Options to configure vocab parallel loss computation
+
+    :param start: The start index for this rank's range in the vocab.
+    :param end: The ending index (non-inclusive)
+    :param group: The distributed process group defining the world for this vocab parallel rank.
+    :param reduce_e_grad: Whether or not to all_reduce/synchronize the gradient for the embedding
+        matrix across all ranks. This typically should be true, but some frameworks may require setting this to false.
+    """
+
     start: int
     stop: int
     group: torch.distributed.ProcessGroup | None = None
+    reduce_e_grad: bool = True
 
     @classmethod
     def from_vocab(
-        cls, vocab_size: int, group: torch.distributed.ProcessGroup | None = None
+        cls,
+        vocab_size: int,
+        group: torch.distributed.ProcessGroup | None = None,
+        reduce_e_grad: bool = True,
     ) -> Self:
         rank = torch.distributed.get_rank(group)
         world_size = torch.distributed.get_world_size(group)
 
         start, stop = partition_n_into_range(vocab_size, rank, world_size)
 
-        return cls(start, stop, group)
+        return cls(start, stop, group, reduce_e_grad)
 
 
 @torch.compile(fullgraph=True)
@@ -50,3 +63,36 @@ def vp_reduce_correct_logit(
     correct_logit = vp_correct_logit.to(dtype=dtype, copy=True)
     torch.distributed.all_reduce(correct_logit, group=pg)
     return correct_logit
+
+
+@torch.compile(fullgraph=True)
+def vp_reduce_e_grad(
+    e_grad: torch.Tensor,
+    pg: torch.distributed.ProcessGroup | None,
+) -> torch.Tensor:
+    reduced_grad = e_grad.to(dtype=torch.float32, copy=True)
+    torch.distributed.all_reduce(reduced_grad, group=pg)
+
+    return reduced_grad.type_as(e_grad)
+
+
+class _VocabParallelReduceEGradHook(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx, e: torch.Tensor, pg: torch.distributed.ProcessGroup | None = None
+    ) -> torch.Tensor:
+        ctx.pg = pg
+        return e
+
+    @staticmethod
+    def backward(ctx, e_grad: torch.Tensor) -> tuple[torch.Tensor, None]:
+        return vp_reduce_e_grad(e_grad, ctx.pg), None
+
+
+def vp_reduce_e_grad_hook(
+    e: torch.Tensor, vocab_parallel_options: VocabParallelOptions
+) -> torch.Tensor:
+    if vocab_parallel_options.reduce_e_grad:
+        return _VocabParallelReduceEGradHook.apply(e, vocab_parallel_options.group)
+    else:
+        return e

tests/test_vocab_parallel.py
@@ -24,7 +24,13 @@ def find_free_port() -> int:
 
 
 def _target_fn_test_vp(
-    rank: int, world_size: int, port: int, dtype: torch.dtype, error_tol: float, invalids: bool
+    rank: int,
+    world_size: int,
+    port: int,
+    impl: str,
+    dtype: torch.dtype,
+    error_tol: float,
+    invalids: bool,
 ):
     device = (
         torch.device("cpu")
@@ -46,8 +52,6 @@ def _target_fn_test_vp(
         backend=backend, store=store, world_size=world_size, rank=rank
     )
 
-    torch.distributed.init_process_group()
-
     N, V, D = (252, 507, 123)
 
     e = torch.randn((N, D), device=device, dtype=dtype) / (D**0.5)
@@ -61,16 +65,27 @@ def _target_fn_test_vp(
     e = e.view(4, -1, D)
     targets = targets.view(e.size()[0:-1])
 
+    torch.distributed.broadcast(e, src=0)
+    torch.distributed.broadcast(c, src=0)
+    torch.distributed.broadcast(targets, src=0)
+
     vocab_parallel_options = VocabParallelOptions.from_vocab(V)
 
     vp_c = c[vocab_parallel_options.start : vocab_parallel_options.stop].clone()
 
     vp_c.requires_grad_(True)
-    vp_loss = linear_cross_entropy(e, vp_c, targets, vocab_parallel_options=vocab_parallel_options)
+    e.requires_grad_(True)
+    vp_loss = linear_cross_entropy(
+        e, vp_c, targets, impl=impl, vocab_parallel_options=vocab_parallel_options
+    )
     vp_loss.backward()
 
+    assert e.grad is not None
+    vp_e_grad = e.grad.clone()
+    e.grad = None
+
     c.requires_grad_(True)
-    loss = linear_cross_entropy(e, c, targets)
+    loss = linear_cross_entropy(e, c, targets, impl=impl)
     loss.backward()
 
     assert c.grad is not None
@@ -79,10 +94,17 @@ def _target_fn_test_vp(
         c.grad[vocab_parallel_options.start : vocab_parallel_options.stop],
         vp_c.grad,
         atol=error_tol,
-    )
+    ), "c grad not close"
+
+    assert e.grad is not None
+    assert torch.allclose(
+        e.grad,
+        vp_e_grad,
+        atol=error_tol,
+    ), f"{(e.grad - vp_e_grad).abs().max().item()=}"
 
 
-@pytest.mark.parametrize("impl", ["torch_compile", "cce"])
+@pytest.mark.parametrize("impl", ["torch_compile", "cce_exact"])
 @pytest.mark.parametrize("dtype,error_tol", [(torch.float16, 1e-3), (torch.bfloat16, 1e-2)])
 @pytest.mark.parametrize("nprocs", [4])
 @pytest.mark.parametrize("invalids", [False, True])
@@ -94,7 +116,7 @@ def test_vocab_parallel(
 
     mp_spawn(
         _target_fn_test_vp,
-        args=(nprocs, find_free_port(), dtype, error_tol, invalids),
+        args=(nprocs, find_free_port(), impl, dtype, error_tol, invalids),
         nprocs=nprocs,
         join=True,
     )